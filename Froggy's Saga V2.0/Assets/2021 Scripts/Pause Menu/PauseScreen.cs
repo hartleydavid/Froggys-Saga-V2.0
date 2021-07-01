32213a4293606d6b6442c4bf08867213358ca575
hartleydavid <56169730+hartleydavid@users.noreply.github.com>
2021-06-30 23:19:47 -0700

    Cleaned up code for pause screen menus to be much more simple. Settings menu needs functionality
    Code used for testing was a mess and most methods that I had were useless and never used in the end. Settings screen buttons and scrollbar need functionality to effect the game, other than that the settings sub menu in the pause screen works and transitions properly.

--- Froggy's Saga V2.0/Assets/2021 Scripts/Pause Menu/PauseScreen.cs
+++ Froggy's Saga V2.0/Assets/2021 Scripts/Pause Menu/PauseScreen.cs
@@ -31,41 +31,6 @@ public class PauseScreen : MonoBehaviour
         }
     }
 
-    /* Transitions the current tab in the pause screen, that being info, settings, Hub world and quit
-     * Polymorphic so any one of these "tabs" can use the method 
-     * @param RectTransform rect: The rectangle Transform of the tab to be transitioned.
-     *                            --MUST be a Full Screen stretch transform to allow the method to work--
-     * @param Animator animator: The animator the Tab uses for its transition
-     * @param Vector2 max: the Right and Top coords for the transform to move to, must be ~new Vector2(-right, -top);
-     * @param Vector2 min: The left and bottom coords for the transform to move to, must be ~new Vector2(left, bottom);
-     * @param string trigger: The name of the Trigger to set to allow for the correct animation 
-     *                        Two options: "Open" which opens the tab and "Close" which puts the tab away
-     *                        The Triggers for the animator Parameters must be called this
-     */
-    public void Transition(RectTransform rect, Animator animator, string trigger)
-    {
-
-        //Play the animation
-        animator.SetTrigger(trigger);
-        //Sets the animators Param for when you close the tab to transition to empty state
-        bool isOpen = trigger.Equals("Open") ? OnScreen(rect) : OffScreen(rect);
-        animator.SetBool("IsOpen", isOpen);
-    }
-
-    private bool OnScreen(RectTransform rect)
-    {
-        rect.offsetMax = new Vector2(0f, -181.3221f);//max
-        rect.offsetMin = new Vector2(407.5f, 0f);//min
-        return true;
-    }
-
-    private bool OffScreen(RectTransform rect)
-    {
-        rect.offsetMax = new Vector2(1617.25f, -181.3221f);//max
-        rect.offsetMin = new Vector2(2024.75f, 0f); //min
-        return false;
-    }
-
     private void ChangeAplhaValue(float newAlphaValue)
     {
         var tempColor = pauseScreenImage.color;
--- Froggy's Saga V2.0/Assets/2021 Scripts/Pause Menu/PauseScreen_Menus.cs
+++ Froggy's Saga V2.0/Assets/2021 Scripts/Pause Menu/PauseScreen_Menus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PauseScreen_Menus : MonoBehaviour
 {
-    public RectTransform rectTransform;
+   // public RectTransform rectTransform;
     public Animator animator;
     public PauseScreen paused;
     private bool isOpen;
@@ -13,63 +13,27 @@ public class PauseScreen_Menus : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //paused = FindObjectOfType<PauseScreen>();
-        //Start off screen!
-        rectTransform.offsetMax = new Vector2(1617.25f, -181.3221f);//max
-        rectTransform.offsetMin = new Vector2(2024.75f, 0f); //min
         isOpen = false;
     }
 
-    /* Transitions the current tab in the pause screen, that being info, settings, Hub world and quit
- * Polymorphic so any one of these "tabs" can use the method 
- * @param RectTransform rect: The rectangle Transform of the tab to be transitioned.
- *                            --MUST be a Full Screen stretch transform to allow the method to work--
- * @param Animator animator: The animator the Tab uses for its transition
- * @param Vector2 max: the Right and Top coords for the transform to move to, must be ~new Vector2(-right, -top);
- * @param Vector2 min: The left and bottom coords for the transform to move to, must be ~new Vector2(left, bottom);
- * @param string trigger: The name of the Trigger to set to allow for the correct animation 
- *                        Two options: "Open" which opens the tab and "Close" which puts the tab away
- *                        The Triggers for the animator Parameters must be called this
- */
-    public void Transition(string trigger)
+    public void OnClickEvent()
     {
-        //Play the animation
-        animator.SetTrigger(trigger);
-        //Sets the animators Param for when you close the tab to transition to empty state
-        bool isOpen = trigger.Equals("Open") ? OnScreen() : OffScreen();
-        animator.SetBool("IsOpen", isOpen);
+        if (!isOpen)
+        {
+            MenuEvent("Open", true);
+            //paused.CloseAllTabs();
+        }
+        else
+        {
+            MenuEvent("Close", false);
+        }
+        isOpen = !isOpen;
     }
 
-    private bool OnScreen()
-    {
-        rectTransform.offsetMax = new Vector2(0f, -181.3221f);//max
-        rectTransform.offsetMin = new Vector2(407.5f, 0f);//min
-        return true;
-    }
 
-    private bool OffScreen()
+    private void MenuEvent(string triggerKey, bool booleanKey)
     {
-        rectTransform.offsetMax = new Vector2(1617.25f, -181.3221f);//max
-        rectTransform.offsetMin = new Vector2(2024.75f, 0f); //min
-        return false;
+        animator.SetTrigger(triggerKey);
+        animator.SetBool("IsOpen", booleanKey);
     }
-
-    public void OpenMenu()
-    {
-        if (isOpen) return; //if its open you cant open it again, exit case prevents animation replay               
-        //UpdateInfo(FindObjectOfType<GameManager>().GetPlayersCharacter());
-        Transition("Open");
-        //paused.Transition(rectTransform, animator, "Open");// close tab
-        isOpen = true; //paused.CloseAllTabs();
-    }
-
-    //Closes the settings tab in the pause Screen
-    public void CloseMenu()
-    {
-        if (!isOpen) return; //if its closed you cant close it again, exit case prevents animation replay
-        //paused.Transition(rectTransform, animator, "Close");// close tab
-        Transition("Close");
-        isOpen = false;
-    }
-
 }
